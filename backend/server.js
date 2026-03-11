b2a9afa71f5f6b19bd008d41189d99a02618c938
Mishell Cardenas <cardenasespinosa.m@northeastern.edu>
2026-03-11 18:25:34 -0400

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import "dotenv/config";
+import express from "express";
+import { connectDB } from "./db/connection.js";
+
+// TEST ROUTES: TO UPDATE AS WE PROGRESS
+import profileRoutes from "./routes/profileRoutes.js";
+import trackedRepoRoutes from "./routes/trackedRepoRoutes.js";
+import contributionRoutes from "./routes/contributionRoutes.js";
+import githubRoutes from "./routes/githubRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+async function startServer() {
+  try {
+    // connect to MongoDB
+    const db = await connectDB();
+
+    // attach db to every request
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
+
+    // TEST ROUTES: TO UPDATE AS WE PROGRESS
+    app.use("/api/profiles", profileRoutes);
+    app.use("/api/tracked-repos", trackedRepoRoutes);
+    app.use("/api/contributions", contributionRoutes);
+    app.use("/api/github", githubRoutes);
+
+    // test route
+    app.get("/", (req, res) => {
+      res.send("RepoScout backend is running");
+    });
+
+    // start server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
